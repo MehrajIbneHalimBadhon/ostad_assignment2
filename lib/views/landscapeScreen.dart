b8b5b5af5927419f2e21171ad9e53a49ccc82794
MehrajBadon <abdulhalim018491@gmail.com>
2024-04-26 22:24:08 +0600

    updated

--- lib/views/landscapeScreen.dart
+++ lib/views/landscapeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ostad_assignment2/views/portraitScreen.dart';
 import 'package:ostad_assignment2/widget/custom_button.dart';
 
 import '../model/photo_gallery.dart';
@@ -70,6 +71,7 @@ class _LandScapeScreenState extends State<LandScapeScreen> {
   }
 }
 
+
 class LandScapeDetailsScreen extends StatelessWidget {
   final PhotoGallery gallery;
 
@@ -77,119 +79,125 @@ class LandScapeDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(gallery.title), centerTitle: true),
-      body: SingleChildScrollView(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Flexible(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: customImage(
-                  gallery.thumbnail,
-                ),
-              ),
-            ),
-            Flexible(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    headLine(
-                      gallery.title,
-                    ),
-                    customText(
-                      gallery.description,
-                    ),
-                    SizedBox(height: 5),
-                    customButton('See More', () {}),
-                    SizedBox(height: 5),
-                    headLine(gallery.suggestion),
-                    SizedBox(height: 5),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 5),
-                      child: Container(
-                        height: 200,
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.only(right: 2.5),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(12),
-                                    image: DecorationImage(
-                                      image:
-                                          AssetImage(gallery.suggestionImage1),
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                  child: Stack(
-                                    children: [
-                                      Positioned(
-                                        bottom: 10,
-                                        left: 10,
-                                        child: Text(
-                                          'Dawn',
-                                          style: const TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Expanded(
-                              child: Padding(
-                                padding: const EdgeInsets.only(left: 2.5),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(12),
-                                    image: DecorationImage(
-                                      image:
-                                          AssetImage(gallery.suggestionImage2),
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                  child: Stack(
-                                    children: [
-                                      Positioned(
-                                        bottom: 10,
-                                        left: 10,
-                                        child: Text(
-                                          'Leaves',
-                                          style: const TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 16,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
+    return Builder(
+      builder: (BuildContext context) {
+        if (MediaQuery.of(context).orientation == Orientation.landscape) {
+          return Scaffold(
+            appBar: AppBar(title: Text(gallery.title), centerTitle: true),
+            body: OrientationBuilder(
+              builder: (context, orientation) {
+                return SingleChildScrollView(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Flexible(
+                        flex: 1,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: customImage(gallery.thumbnail),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
+                      Flexible(
+                        flex: 1,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              headLine(gallery.title),
+                              customText(gallery.description),
+                              SizedBox(height: 5),
+                              customButton('See More', () {}),
+                              SizedBox(height: 5),
+                              headLine(gallery.suggestion),
+                              SizedBox(height: 5),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 5),
+                                child: Container(
+                                  height: 200,
+                                  child: Row(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Expanded(
+                                        child: Padding(
+                                          padding: const EdgeInsets.only(right: 2.5),
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.circular(12),
+                                              image: DecorationImage(
+                                                image: AssetImage(gallery.suggestionImage1),
+                                                fit: BoxFit.cover,
+                                              ),
+                                            ),
+                                            child: Stack(
+                                              children: [
+                                                Positioned(
+                                                  bottom: 10,
+                                                  left: 10,
+                                                  child: Text(
+                                                    'Dawn',
+                                                    style: const TextStyle(
+                                                      color: Colors.white,
+                                                      fontSize: 16,
+                                                      fontWeight: FontWeight.bold,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: Padding(
+                                          padding: const EdgeInsets.only(left: 2.5),
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                              borderRadius: BorderRadius.circular(12),
+                                              image: DecorationImage(
+                                                image: AssetImage(gallery.suggestionImage2),
+                                                fit: BoxFit.cover,
+                                              ),
+                                            ),
+                                            child: Stack(
+                                              children: [
+                                                Positioned(
+                                                  bottom: 10,
+                                                  left: 10,
+                                                  child: Text(
+                                                    'Leaves',
+                                                    style: const TextStyle(
+                                                      color: Colors.white,
+                                                      fontSize: 16,
+                                                      fontWeight: FontWeight.bold,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+          );
+        } else {
+          return PortraitDetailsScreen(gallery: gallery);
+        }
+      },
     );
   }
 }
+
+
--- lib/views/portraitScreen.dart
+++ lib/views/portraitScreen.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:ostad_assignment2/const/app_colors.dart';
+import 'package:ostad_assignment2/views/landscapeScreen.dart';
 
 import '../model/photo_gallery.dart';
 import '../widget/custom_button.dart';
@@ -71,7 +72,6 @@ class _PortraitScreenState extends State<PortraitScreen> {
     );
   }
 }
-
 class PortraitDetailsScreen extends StatelessWidget {
   final PhotoGallery gallery;
 
@@ -79,115 +79,132 @@ class PortraitDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(gallery.title), centerTitle: true,),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: customImage(
-                gallery.thumbnail,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  headLine(
-                    gallery.title,
-                  ),
-                  customText(
-                    gallery.description,
-                  ),
-                  SizedBox(height: 10),
-                  customButton('See More', (){}),
-                  SizedBox(height: 10),
-                  Text(
-                    gallery.suggestion,
-                    style: TextStyle(color: AppColors.greenColor, fontSize: 20, fontWeight: FontWeight.w500),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: Container(
-                      height: 200,
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.only(right: 2.5),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(12),
-                                  image: DecorationImage(
-                                    image: AssetImage(gallery.suggestionImage1),
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                                child: Stack(
-                                  children: [
-                                    Positioned(
-                                      bottom: 10,
-                                      left: 10,
-                                      child: Text(
-                                        'Dawn',
-                                        style: const TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.only(left: 2.5),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(12),
-                                  image: DecorationImage(
-                                    image: AssetImage(gallery.suggestionImage2),
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                                child: Stack(
-                                  children: [
-                                    Positioned(
-                                      bottom: 10,
-                                      left: 10,
-                                      child: Text(
-                                        'Leaves',
-                                        style: const TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
+    return Builder(
+      builder: (BuildContext context) {
+        if (MediaQuery.of(context).orientation == Orientation.portrait) {
+          return Scaffold(
+            appBar: AppBar(title: Text(gallery.title), centerTitle: true),
+            body: OrientationBuilder(
+              builder: (context, orientation) {
+                return SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: customImage(
+                          gallery.thumbnail,
+                        ),
                       ),
-                    ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            headLine(
+                              gallery.title,
+                            ),
+                            customText(
+                              gallery.description,
+                            ),
+                            SizedBox(height: 10),
+                            customButton('See More', () {}),
+                            SizedBox(height: 10),
+                            Text(
+                              gallery.suggestion,
+                              style: TextStyle(
+                                color: AppColors.greenColor,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 10),
+                              child: Container(
+                                height: 200,
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Expanded(
+                                      child: Padding(
+                                        padding: const EdgeInsets.only(right: 2.5),
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(12),
+                                            image: DecorationImage(
+                                              image: AssetImage(gallery.suggestionImage1),
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                          child: Stack(
+                                            children: [
+                                              Positioned(
+                                                bottom: 10,
+                                                left: 10,
+                                                child: Text(
+                                                  'Dawn',
+                                                  style: const TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 16,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Padding(
+                                        padding: const EdgeInsets.only(left: 2.5),
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(12),
+                                            image: DecorationImage(
+                                              image: AssetImage(gallery.suggestionImage2),
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                          child: Stack(
+                                            children: [
+                                              Positioned(
+                                                bottom: 10,
+                                                left: 10,
+                                                child: Text(
+                                                  'Leaves',
+                                                  style: const TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 16,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
+                );
+              },
+            ),
+          );
+        } else {
+          return LandScapeDetailsScreen(gallery: gallery);
+        }
+      },
     );
   }
 }
+
 
